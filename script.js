a2e672154202f1854890d45ef193aad82ff20816
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-23 07:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const panels = document.querySelectorAll(".panel");
+let currentActive= panels[0];
+
+panels.forEach(panel => {
+	panel.addEventListener("click", () => {
+		panel.classList.add("active");
+		currentActive.classList.remove("active");
+		currentActive= panel;
+	})
+})
